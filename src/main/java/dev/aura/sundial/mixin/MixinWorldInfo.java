3e66df1ebada7285bb88ceda61b25395dfa55719
BrainStone <yannick@tedworld.de>
2020-12-04 22:40:23 +0100

    Try more mixin magic

--- src/main/java/dev/aura/sundial/mixin/MixinWorldInfo.java
+++ src/main/java/dev/aura/sundial/mixin/MixinWorldInfo.java
@@ -3,26 +3,47 @@ package dev.aura.sundial.mixin;
 import dev.aura.sundial.AuraSunDial;
 import net.minecraft.world.storage.WorldInfo;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.event.cause.EventContextKeys;
-import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.world.storage.WorldProperties;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
+import org.spongepowered.asm.mixin.Intrinsic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(WorldInfo.class)
-public class MixinWorldInfo {
+@Mixin(value = WorldInfo.class, priority = 1001)
+@Implements(@Interface(iface = WorldProperties.class, prefix = "worldproperties$"))
+public abstract class MixinWorldInfo implements WorldProperties {
+  @Shadow
+  public abstract void shadow$setWorldTime(long time);
+
+  @Intrinsic
+  public void worldproperties$setWorldTime(final long time) {
+    this.shadow$setWorldTime(time);
+
+    AuraSunDial.getLogger().info("worldproperties$setWorldTime: Time set to " + time);
+    AuraSunDial.getLogger()
+        .info(
+            "worldproperties$setWorldTime: "
+                + Sponge.getCauseStackManager().getCurrentCause().toString());
+  }
+
   @Inject(method = "setWorldTime", at = @At("RETURN"))
   public void onSetWorldTime(long time, CallbackInfo ci) {
-    // Skip our own changes
-    if (Sponge.getCauseStackManager()
-        .getContext(EventContextKeys.PLUGIN)
-        .map(PluginContainer::getId)
-        .filter(AuraSunDial.ID::equals)
-        .isPresent()) return;
+    // new NullPointerException().printStackTrace();
 
-    AuraSunDial.getLogger().info("WorldInfo: Time set to " + time);
-    AuraSunDial.getLogger().info(ci.toString());
-    AuraSunDial.getLogger().info(Sponge.getCauseStackManager().getCurrentCause().toString());
+    // Skip our own changes
+    /*if (Sponge.getCauseStackManager()
+    .getContext(EventContextKeys.PLUGIN)
+    .map(PluginContainer::getId)
+    .filter(AuraSunDial.ID::equals)
+    .isPresent()) return;*/
+
+    AuraSunDial.getLogger().info("onSetWorldTime: Time set to " + time);
+    AuraSunDial.getLogger().info("onSetWorldTime: " + ci.toString());
+    AuraSunDial.getLogger()
+        .info("onSetWorldTime: " + Sponge.getCauseStackManager().getCurrentCause().toString());
   }
 }
